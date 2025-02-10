a7c195dd802b5aca5e758867c955a11aa2e13ec3
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2025-02-09 22:18:33 -0500

    Add endian argument to anim import

--- apps/cli/mesh_tool/src/apps/anim.rs
+++ apps/cli/mesh_tool/src/apps/anim.rs
@@ -13,21 +13,27 @@ pub struct AnimApp {
     pub anim_path: String,
     #[arg(help = "Path to output directory", required = true)]
     pub output_path: String,
+    #[arg(short, long, help = "Use big endian serialization")]
+    pub big_endian: bool,
 }
-
-// TODO: Get from args
-const SYSTEM_INFO: SystemInfo = SystemInfo {
-    version: 25,
-    platform: Platform::X360,
-    endian: IOEndian::Little,
-};
 
 impl SubApp for AnimApp {
     fn process(&mut self) -> Result<(), Box<dyn Error>> {
+        // TODO: Get from args
+        let system_info: SystemInfo = SystemInfo {
+            version: 25,
+            platform: Platform::X360,
+            endian: if self.big_endian {
+                IOEndian::Big
+            } else {
+                IOEndian::Little
+            },
+        };
+
         let importer = GltfImporter2::new(&self.anim_path)?;
         let assets = importer.process();
 
-        assets.dump_to_directory(&self.output_path, &SYSTEM_INFO)?;
+        assets.dump_to_directory(&self.output_path, &system_info)?;
 
         Ok(())
     }
